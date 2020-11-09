03d5ae6008c3491676b099e6412babf1fcdbab0c
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-11-09 14:11:37 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-require('update-electron-app'){
+require('update-electron-app')({
   repo: '',
   updateInterval: '5 minutes',
   logger: require('electron-log')
-}
+})
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
